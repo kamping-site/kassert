e49c7fad57c5d6aafa56724b1e8f60d32fe286eb
Tim Niklas Uhl <niklas.uhl@online.de>
2025-02-09 22:27:15 +0100

    Modernize CMake and fix compiler defines. Make installable. (#48)
    * Add warning flags via target instead of variable.
    
    * Better CMake dependency management.
    
    * Formatting.
    
    * Remove some old cruft.
    
    * Better handling of assertions levels and exception mode via CMake properties.
    
    * CMake installation.
    
    * Update README.
    
    * Emulate `PROJECT_IS_TOP_LEVEL` for CMake < 3.21.
    
    * Fix include guard.
    
    * CMake formatting.

--- tests/cmake/KassertTestHelper.cmake
+++ tests/cmake/KassertTestHelper.cmake
@@ -1,4 +1,12 @@
-add_subdirectory("${PROJECT_SOURCE_DIR}/extern/googletest" "extern/googletest")
+include(FetchContent)
+
+set(INSTALL_GTEST OFF)
+FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest
+    GIT_TAG v1.15.2
+)
+FetchContent_MakeAvailable(googletest)
 
 include(GoogleTest)
 
@@ -9,12 +17,12 @@ include(GoogleTest)
 function (kassert_register_test KASSERT_TARGET_NAME)
     cmake_parse_arguments("KASSERT" "EXCEPTION_MODE" "" "FILES" ${ARGN})
     add_executable(${KASSERT_TARGET_NAME} ${KASSERT_FILES})
-    target_link_libraries(${KASSERT_TARGET_NAME} PRIVATE gtest gtest_main gmock kassert_base)
-    target_compile_options(${KASSERT_TARGET_NAME} PRIVATE ${KASSERT_WARNING_FLAGS})
-    gtest_discover_tests(${KASSERT_TARGET_NAME} WORKING_DIRECTORY ${PROJECT_DIR})
+    target_link_libraries(${KASSERT_TARGET_NAME} PRIVATE gtest gtest_main gmock kassert)
+    target_link_libraries(${KASSERT_TARGET_NAME} PRIVATE kassert_warnings)
+    gtest_discover_tests(${KASSERT_TARGET_NAME})
 
     if (KASSERT_EXCEPTION_MODE)
-        target_compile_options(${KASSERT_TARGET_NAME} PRIVATE -DKASSERT_EXCEPTION_MODE)
+        set_target_properties(${KASSERT_TARGET_NAME} PROPERTIES KASSERT_EXCEPTION_MODE ON)
     endif ()
 endfunction ()
 
