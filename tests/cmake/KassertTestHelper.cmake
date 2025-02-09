6839b14d09c47d89c51fc96957d033e7ab1e3497
Niklas Uhl <niklas.uhl@online.de>
2025-02-09 14:15:36 +0100

    CMake formatting.

--- tests/cmake/KassertTestHelper.cmake
+++ tests/cmake/KassertTestHelper.cmake
@@ -2,12 +2,11 @@ include(FetchContent)
 
 set(INSTALL_GTEST OFF)
 FetchContent_Declare(
-  googletest
-  GIT_REPOSITORY https://github.com/google/googletest
-  GIT_TAG v1.15.2
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest
+    GIT_TAG v1.15.2
 )
 FetchContent_MakeAvailable(googletest)
-
 
 include(GoogleTest)
 
